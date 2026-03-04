52f12324cbb47a1e300c86c93c2c89cc1cb9ca18
isuri54 <154318794+isuri54@users.noreply.github.com>
2026-03-05 01:01:08 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -10,8 +10,7 @@ export default function Home() {
       {/* Navbar */}
       <div className="flex items-center justify-between border-b pb-4 px-8 pt-6 bg-white">
         <div className="flex items-center">
-          <img src="/agri.png" alt="Agriscope Logo" className="h-20 w-20 object-contain" />
-          <span className="text-lg font-bold text-green-700 ml-2">Agriscope</span>
+          <img src="/logo.png" alt="Agriscope Logo" className="h-30 w-60 object-contain" />
         </div>
 
         <div className="flex items-center gap-8 text-sm font-medium">
